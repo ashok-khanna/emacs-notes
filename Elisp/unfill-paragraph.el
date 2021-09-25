259cbd5e1da24be9d8d17a5ea5c71980ef946415
Ashok Khanna <46754667+ashok-khanna@users.noreply.github.com>
2021-09-26 02:41:37 +0400

    Create unfill-paragraph.el

---
+++ Elisp/unfill-paragraph.el
@@ -0,0 +1,8 @@
+;; Unfill Paragraph
+
+;;; It is the opposite of fill-paragraph    
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
